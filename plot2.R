8932adfe7f1e3a14fd6544308b11ad4da9a65f00
roystonhuan <royston.huan@gmail.com>
2015-12-13 03:17:05 -0400

    Plots

---
+++ plot2.R
@@ -0,0 +1,25 @@
+#Preliminary data subsetting work
+#Reads TXT file from working directory
+#Creates a new column called DateAndTime combining the first two columns
+#New column called DateAndTime2 created as POSIX format from DateAndTime column (character class)
+#New column called Date2 created (different format from Date column)
+
+Power<-read.table("household_power_consumption.txt",header = TRUE, sep=";")
+Power$DateAndTime = paste(Power$Date,Power$Time,sep=" ")
+x<-strptime(Power$DateAndTime, "%d/%m/%Y %H:%M:%S")
+Power$DateAndTime2 = x
+y<-as.Date(Power$Date,"%d/%m/%Y")
+Power$Date2 = y
+
+#Subset of Data for 1 and 2 Feb 2007 only
+
+a<-Power[Power$Date2 == "2007-02-01",]
+b<-Power[Power$Date2 == "2007-02-02",]
+Subset<-rbind(a,b)
+
+#Plot2
+png(file="plot2.png", width=480, height=480)
+GAP<-as.numeric(as.character(Subset$Global_active_power))
+plot(Subset$DateAndTime2, GAP, main="", type="s",  frame.plot="TRUE", ylab="Global Active Power (kilowatts)",xlab="" )
+dev.off()
+
---
+++ plot3.R
@@ -0,0 +1,28 @@
+#Preliminary data subsetting work
+#Reads TXT file from working directory
+#Creates a new column called DateAndTime combining the first two columns
+#New column called DateAndTime2 created as POSIX format from DateAndTime column (character class)
+#New column called Date2 created (different format from Date column)
+
+Power<-read.table("household_power_consumption.txt",header = TRUE, sep=";")
+Power$DateAndTime = paste(Power$Date,Power$Time,sep=" ")
+x<-strptime(Power$DateAndTime, "%d/%m/%Y %H:%M:%S")
+Power$DateAndTime2 = x
+y<-as.Date(Power$Date,"%d/%m/%Y")
+Power$Date2 = y
+
+#Subset of Data for 1 and 2 Feb 2007 only
+
+a<-Power[Power$Date2 == "2007-02-01",]
+b<-Power[Power$Date2 == "2007-02-02",]
+Subset<-rbind(a,b)
+
+#Plot3
+png(file="plot3.png", width=480, height=480)
+plot(Subset$DateAndTime2, as.numeric(as.character(Subset$Sub_metering_1)), main="", type="n",frame.plot="TRUE",ylab="Energy sub metering",xlab="")
+points(Subset$DateAndTime2, as.numeric(as.character(Subset$Sub_metering_1)),type="s")
+points(Subset$DateAndTime2, as.numeric(as.character(Subset$Sub_metering_2)),type="s",col="red")
+points(Subset$DateAndTime2, as.numeric(as.character(Subset$Sub_metering_3)),type="s",col="blue")
+legend("topright", lty=c(1,1,1), col=c("black","red","blue"), c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
+
